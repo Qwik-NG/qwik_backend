a9d713bdf1ae4252e40011c37f30e99e5eda20d3
Abraham Ogbu <243545791+Abraham3stack@users.noreply.github.com.>
2026-09-08 14:33:24 +0300

    chore: stop tracking generated dist output

--- dist/middleware/requestLogger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.requestLogger = requestLogger;
-function requestLogger(req, _res, next) {
-    console.log(`${new Date().toISOString()} ${req.method} ${req.path}`);
-    next();
-}
